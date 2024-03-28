7dcb032112667013378a4bb26af1efeca5023ea4
AbhishekNanda7429 <v-abhishek.nanda@kfintech.com>
2024-03-28 12:16:12 +0530

    v2 intial

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my_package',
-    version='1.0.0',
+    version='2.0.0',
     description='Package for taking JSON input and uploading to S3',
     packages=find_packages(),
     install_requires=['boto3'],
